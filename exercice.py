675ff13e7339cf31061c7d4d575c0fac41e3db45
Alice-Poly <70984340+Alice-Poly@users.noreply.github.com>
2020-11-10 19:26:40 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -18,6 +18,16 @@ def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
 def find_closest_index(values: np.ndarray, number: float) -> int:
     return 0
 
+def integral() -> tuple:
+    result_inf = integrate.quad(lambda x: np.exp(-x ** 2), -np.inf, +np.inf)
+
+    x = np.arange(-4, 4, 0.1)
+
+    y = [integrate.quad(lambda x: np.exp(-x ** 2), 0, value)[0] for value in x]
+    plt.plot(x, y)
+    plt.show()
+    return result_inf
+
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
